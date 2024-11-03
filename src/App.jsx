89b837f9bda05de96bce76136b31683aa8f8a94b
AliMohamed <137190242+AliMohamed35@users.noreply.github.com>
2024-11-02 20:38:07 -0700

    Configuring SEO

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Todo from "./TodoList";
 function App() {
   return (
     <>
+      <h1 style={{ fontSize: "0.7rem", fontWeight: "100" }}>
+        Copyrights © Ali Mohamed
+      </h1>
       <Todo />
     </>
   );
